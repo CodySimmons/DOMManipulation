ea305db1439c072e98836b65265ab672ad151481
Cody Simmons <cdysmmns@gmail.com>
2021-01-26 12:44:27 -0700

    dom manip exercise completed.

--- DOMManupulation.js
+++ DOMManupulation.js
@@ -1,26 +1,26 @@
 // your javascript file
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
-const content = document.createElement('div');
-content.classList.add('content');
-content.textContent = 'This is the glorious text-content!';
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
 
 //make a <p> with red text
-const pTag = document.createElement('p');
-pTag.style.color = 'red';
-pTag.innerText = "Hey I'm red!"
+const pTag = document.createElement("p");
+pTag.style.color = "red";
+pTag.innerText = "Hey I'm red!";
 
-const h3Tag = document.createElement('h3');
-h3Tag.style.cssText = 'color: blue';
+const h3Tag = document.createElement("h3");
+h3Tag.style.cssText = "color: blue";
 h3Tag.innerText = "I'm a blue h3!";
 
-const divPink = document.createElement('div');
-divPink.style.cssText = 'background-color: pink; border: medium solid black';
+const divPink = document.createElement("div");
+divPink.style.cssText = "background-color: pink; border: medium solid black";
 
-const h1Tag = document.createElement('h1');
+const h1Tag = document.createElement("h1");
 h1Tag.innerText = "I'm in a div";
 
-const pTagDiv = document.createElement('p');
+const pTagDiv = document.createElement("p");
 pTagDiv.innerText = "ME TOO!";
 
 container.appendChild(content);
